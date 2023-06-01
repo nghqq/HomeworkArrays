6efbc8c8a34de05adb1b260f6a38adf0087e68a7
- <-@DESKTOP-5LUH4K3>
2023-06-01 07:04:53 +0300

    unicode

---
+++ bin_2/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int decimal;
+	cout << "Введите десятичное число: "; cin >> decimal;
+	const int MAX_BIN_CAPACITY = 32;
+	bool bin[MAX_BIN_CAPACITY] = {};
+	int i = 0;
+
+	while (decimal)
+	{
+		bin[i++] = decimal % 2;
+		decimal /= 2;
+
+	}
+
+	for (i--; i >= 0; i--)
+	{
+		cout << bin[i];
+
+		if (i % 8 == 0) cout << " ";
+		if (i % 4 == 0) cout << " ";
+
+
+	}
+}
